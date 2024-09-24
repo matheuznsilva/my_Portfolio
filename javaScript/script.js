9266eab7dde2834cd94e3020c614106c23282cd0
Matheus N. Silva <matheuznsilva@yahoo.com.br>
2024-09-24 03:54:56 -0300

    c15 - add Skill section and bug fix

--- javaScript/script.js
+++ javaScript/script.js
@@ -23,3 +23,42 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+// --------- Technical skills transition
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const bars = document.querySelectorAll('.skill-bar .bar span');
+    // Usar um timeout para garantir que a transição ocorra
+    bars.forEach(bar => {
+        const finalWidth = bar.style.width; // Captura a largura final
+        bar.style.width = '0%'; // Reseta a largura
+        setTimeout(() => {
+            bar.style.width = finalWidth; // Define a largura final após um curto atraso
+        }, 10); // Delay para garantir a transição
+    });
+});
+
+
+// --------- circle skills  transition
+
+document.querySelectorAll('.circle').forEach(circle => {
+    const dots = circle.getAttribute('data-dots');
+    const percent = circle.getAttribute('data-percent');
+    const angle = (360 / dots) * (percent / 100);
+
+    for (let i = 0; i < dots; i++) {
+        const point = document.createElement('div');
+        point.classList.add('points');
+        point.style.setProperty('--i', i);
+        point.style.setProperty('--rot', `${angle}deg`);
+        circle.appendChild(point);
+    }
+
+    // Marcar os pontos conforme a porcentagem
+    const points = circle.querySelectorAll('.points');
+    for (let i = 0; i < percent/*markedPoints*/; i++) {
+        points[i].classList.add('marked');
+    }
+});
